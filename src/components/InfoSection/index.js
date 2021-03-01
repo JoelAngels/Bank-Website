26a71de9b4fc73655d6740d14dcbb80cac7a952e
JoelAngels <joelbaraka4325@gmail.com>
2021-03-01 10:34:59 +0300

    Structured the InfoSEC ELEMENTS

---
+++ src/components/InfoSection/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  InfoContainer,
+  InfoWrapper,
+  InfoRow,
+  Column1,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  BtnWrap,
+} from "./InfoElements";
+import { Button } from "../ButtonElement";
+
+const InfoSection = () => {
+  return (
+    <>
+      <InfoContainer id={id}>
+        <InfoWrapper>
+          <InfoRow>
+            <Column1>
+              <TextWrapper>
+                <TopLine>TopLine</TopLine>
+                <Heading>Heading</Heading>
+                <Subtitle>Subtitle</Subtitle>
+                <BtnWrap>
+                  <Button to="home">Button</Button>
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+          </InfoRow>
+        </InfoWrapper>
+      </InfoContainer>
+    </>
+  );
+};
+
+export default InfoSection;
