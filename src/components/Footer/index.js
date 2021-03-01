4df88ba1cf7ddff96cfb027207481200bb582c2c
JoelAngels <joelbaraka4325@gmail.com>
2021-03-01 16:32:58 +0300

    layed out the foundation of the footer structure

---
+++ src/components/Footer/index.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  FooterContainer,
+  FooterLinksWrapper,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+} from "./FooterElements";
+
+const Footer = () => {
+  return (
+    <>
+      <FooterContainer>
+        <FooterWrap>
+          <FooterLinksContainer>
+            <FooterLinksWrapper>
+              <FooterLinkItems>
+                <FooterLinkTitle>About Us</FooterLinkTitle>
+                <FooterLink to="/signin">How it works</FooterLink>
+                <FooterLink to="/signin">Testimonial</FooterLink>
+                <FooterLink to="/signin">Careers</FooterLink>
+                <FooterLink to="/signin">Investors</FooterLink>
+                <FooterLink to="/signin">Terms of Services</FooterLink>
+              </FooterLinkItems>
+              <FooterLinkItems>
+                <FooterLinkTitle>About Us</FooterLinkTitle>
+                <FooterLink to="/signin">How it works</FooterLink>
+                <FooterLink to="/signin">Testimonial</FooterLink>
+                <FooterLink to="/signin">Careers</FooterLink>
+                <FooterLink to="/signin">Investors</FooterLink>
+                <FooterLink to="/signin">Terms of Services</FooterLink>
+              </FooterLinkItems>
+            </FooterLinksWrapper>
+            <FooterLinksWrapper>
+              <FooterLinkItems>
+                <FooterLinkTitle>About Us</FooterLinkTitle>
+                <FooterLink to="/signin">How it works</FooterLink>
+                <FooterLink to="/signin">Testimonial</FooterLink>
+                <FooterLink to="/signin">Careers</FooterLink>
+                <FooterLink to="/signin">Investors</FooterLink>
+                <FooterLink to="/signin">Terms of Services</FooterLink>
+              </FooterLinkItems>
+              <FooterLinkItems>
+                <FooterLinkTitle>About Us</FooterLinkTitle>
+                <FooterLink to="/signin">How it works</FooterLink>
+                <FooterLink to="/signin">Testimonial</FooterLink>
+                <FooterLink to="/signin">Careers</FooterLink>
+                <FooterLink to="/signin">Investors</FooterLink>
+                <FooterLink to="/signin">Terms of Services</FooterLink>
+              </FooterLinkItems>
+            </FooterLinksWrapper>
+          </FooterLinksContainer>
+        </FooterWrap>
+      </FooterContainer>
+    </>
+  );
+};
+
+export default Footer;
