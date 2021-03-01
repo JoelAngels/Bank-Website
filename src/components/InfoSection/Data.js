dde8b4c170c5b1ea69455e34b8cdf0b53dc1e5ad
JoelAngels <joelbaraka4325@gmail.com>
2021-03-01 12:18:53 +0300

    Implmeneted and add the styled for the data sec

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+export const homeObjectOne = {
+  id: "about", //this is literally the about section after the Hero section
+  lightBg: false, //making it become dark
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headLine: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged and fees",
+  buttonLabel: "Get Started",
+  imgStart: false, //this come second cause the image is in the right side which is second
+  img: require("../../images/svg-1.svg").default,
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
